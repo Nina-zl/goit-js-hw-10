4d9d843017e0dbd776ccb5a1eea5fe1f41e20dde
Nina-zl <sidorchenkonatasha@gmail.com>
2025-04-04 13:04:26 +0300

    v6

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,40 +1,50 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-const form = document.querySelector(".form");
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector(".form");
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-    const delay = Number(form.elements.delay.value);
-    const state = form.elements.state.value;
+        const delay = Number(form.elements.delay.value);
+        const state = form.elements.state.value;
 
-    if (!delay || !state) {
-        iziToast.warning({
-            title: "⚠️ Warning",
-            message: "Please fill in all fields!",
-            position: "topRight",
+        if (!delay || !state) {
+            iziToast.warning({
+                title: "⚠️ Warning",
+                message: "Please fill in all fields!",
+                position: "topRight",
+            });
+            return;
+        }
+
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (state === "fulfilled") {
+                    resolve(delay);
+                } else {
+                    reject(delay);
+                }
+            }, delay);
         });
-        return;
-    }
 
-    const promise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            state === "fulfilled" ? resolve(delay) : reject(delay);
-        }, delay);
+        promise
+            .then((delay) => {
+                console.log(`✅ Fulfilled promise in ${delay}ms`);
+                iziToast.success({
+                    title: "✅ Success",
+                    message: `Fulfilled promise in ${delay}ms`,
+                    position: "topRight",
+                });
+            })
+            .catch((delay) => {
+                console.log(`❌ Rejected promise in ${delay}ms`);
+                iziToast.error({
+                    title: "❌ Error",
+                    message: `Rejected promise in ${delay}ms`,
+                    position: "topRight",
+                });
+            });
     });
-
-    promise
-        .then((delay) => {
-            iziToast.success({
-                title: "✅ Success",
-                message: `Fulfilled promise in ${delay}ms`,
-                position: "topRight",
-            });
-        })
-        .catch((delay) => {
-            iziToast.error({
-                title: "❌ Error",
-                message: `Rejected promise in ${delay}ms`,
-                position: "topRight",
-            });
-        });
 });
